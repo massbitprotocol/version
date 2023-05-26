e1801d945d5c150d715274545258e8a8296c1105
Design <duyen@Designs-Mac-mini.local>
2023-05-26 09:13:16 +0700

    chore: update pipeline to modify version zesty

---
+++ modify_version_zesty.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+git reset --hard
+
+git switch release
+
+git fetch -p && git pull
+
+ENV=$1
+SERVICE=$2
+TAG=$3
+
+# Modify the ENV
+case "$ENV" in
+    "dev")
+        ENV=".dev"
+      ;;
+      
+    "stg")
+        ENV=".stg"
+      ;;
+    "prod")
+        ENV=""
+      ;;
+    *)
+      # Default case for unknown service names
+      echo "Unknown env: $ENV"
+      ;;
+esac
+
+# Dir
+ROOT_DIR=$(dirname $(realpath $0))
+
+# Modify the version file based on service
+case "$SERVICE" in
+    "juicy")
+        sed "s/JUICY=.*/JUICY=${TAG}/" ${ROOT_DIR}/version${ENV} > ${ROOT_DIR}/version${ENV}.tmp && mv ${ROOT_DIR}/version${ENV}.tmp ${ROOT_DIR}/version${ENV}
+      ;;
+      
+    "so-zesty-jr")
+        sed "s/ZESTY_SO=.*/ZESTY_SO=${TAG}/" ${ROOT_DIR}/version${ENV} > ${ROOT_DIR}/version${ENV}.tmp && mv ${ROOT_DIR}/version${ENV}.tmp ${ROOT_DIR}/version${ENV}
+      ;;
+    "so-zesty")
+        sed "s/ZESTY_NGINX_SO=.*/ZESTY_NGINX_SO=${TAG}/" ${ROOT_DIR}/version${ENV} > ${ROOT_DIR}/version${ENV}.tmp && mv ${ROOT_DIR}/version${ENV}.tmp ${ROOT_DIR}/version${ENV}
+      ;;
+    "zesty")
+        sed "s/ZESTY=.*/ZESTY=${TAG}/" ${ROOT_DIR}/version${ENV} > ${ROOT_DIR}/version${ENV}.tmp && mv ${ROOT_DIR}/version${ENV}.tmp ${ROOT_DIR}/version${ENV}
+      ;;
+    *)
+      # Default case for unknown service names
+      echo "Unknown service: $SERVICE"
+      ;;
+esac
+
+
+# Push to github
+git commit -am"chore: update $ENV $SERVICE to $TAG"
+
+git push
